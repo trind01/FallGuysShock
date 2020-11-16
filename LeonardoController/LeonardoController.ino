1f06ddb438ca2cc4123f8f4247181a704143e483
trind01 <trnd@comcast.net>
2020-11-15 21:59:22 -0600

    Update script and controller

--- LeonardoController/LeonardoController.ino
+++ LeonardoController/LeonardoController.ino
@@ -1,32 +1,100 @@
 #include <XInput.h>
 
-String inputString = "";
+//For Debugging
 int LEDPIN = 8;
 bool LEDLevel = false;
+
+//Control Parsing
+void parseControl(char ctrl);
+void rumbleCallback(uint8_t packetType);
 
 void setup() {
 	XInput.begin();
   Serial1.begin(9600);
   pinMode(LEDPIN,OUTPUT);
   digitalWrite(LEDPIN,LOW);
+
+  // Set callback function. Function must have a 'void' return type
+  // and take a single uint8_t as an argument
+  XInput.setReceiveCallback(rumbleCallback);
 }
 
 void loop() {
-//	XInput.press(BUTTON_A);
-//	delay(1000);
-//
-//	XInput.release(BUTTON_A);
-//	delay(1000);
   if(Serial1.available()){
-    while(Serial1.available())
+    char inChar = (char)Serial1.read(); //read the input
+    parseControl(inChar);
+  }
+  delay(10);
+}
+
+void parseControl(char ctrl)
+{
+  switch(ctrl)
+  {
+    case 'w': //Wake Up Press
     {
-      char inChar = (char)Serial1.read(); //read the input
-      if(inChar == 'e')
+      XInput.press(BUTTON_A);
+      XInput.release(BUTTON_A);
+      break;
+    }
+    case 'j': //Jump
+    {
+      XInput.press(BUTTON_A);
+      XInput.release(BUTTON_A);
+      break;
+    }
+    case 'd': //Dive
+    {
+      XInput.press(BUTTON_X);
+      XInput.release(BUTTON_X);
+      break;
+    }
+    case 'h': //Hold
+    {
+      while(!Serial1.available()){}
+      uint8_t trigger_set = Serial1.read(); //read the input
+      XInput.setTrigger(TRIGGER_RIGHT ,trigger_set);
+      break;
+    }
+    case 'm': //Movement
+    {
+      int16_t x = 0,y = 0;
+      while(!Serial1.available()){}
+      uint8_t xHi = Serial1.read(); //read the input
+      while(!Serial1.available()){}
+      uint8_t xLo = Serial1.read(); //read the input
+      while(!Serial1.available()){}
+      uint8_t yHi = Serial1.read(); //read the input
+      while(!Serial1.available()){}
+      uint8_t yLo = Serial1.read(); //read the input
+      x = (xHi << 8) | xLo;
+      y = (yHi << 8) | yLo;
+      XInput.setJoystick(JOY_LEFT,x,y);
+//      Serial1.write((uint8_t *)x,2);
+//      Serial1.write((uint8_t *)y,2);
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
+}
+
+void rumbleCallback(uint8_t packetType) {
+  // If we have an LED packet (0x01), do nothing
+  if (packetType == (uint8_t) XInputReceiveType::LEDs) {
+    return;
+  }
+
+  // If we have a rumble packet (0x00), see our rumble data on the LED
+  else if (packetType == (uint8_t) XInputReceiveType::Rumble) {
+      uint8_t rumbleLeft = XInput.getRumbleLeft();
+      uint8_t rumbleRight = XInput.getRumbleRight();
+      if(rumbleLeft)
       {
         LEDLevel = !LEDLevel;
         digitalWrite(LEDPIN,LEDLevel);
       }
-    }
   }
-  delay(100);
 }
--- transferScript.py
+++ transferScript.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 #Open ESP Port
 ESPserial = serial.Serial('COM8',115200)
@@ -6,13 +7,37 @@ ESPserial = serial.Serial('COM8',115200)
 #Open Arduion Serial
 ArdSerial = serial.Serial('COM11',9600)
 
-#Write and Read to ESP
-ESPserial.write(b'hello')
-s = ESPserial.read(5)
-print(s)
+#Test Message for testing purposes
+# ESPserial.write(b'hello')
 
-#Write To Arduino
-ArdSerial.write(b'hello')
+#Pass messages between devices
+while True:
+	try:
+		if ESPserial.in_waiting:
+			espser = bytearray()
+			while ESPserial.in_waiting:
+				espser.extend(ESPserial.read(1))
+			# print(espser.hex())
+			ArdSerial.write(espser)
+		if ArdSerial.in_waiting:
+			ardser = bytearray()
+			while ArdSerial.in_waiting:
+				ardser.extend(ArdSerial.read(1))
+			# ESPserial.write(ardser)
+			# print(ardser.hex())
+		time.sleep(0.01)
+	except KeyboardInterrupt:
+		print("End")
+		break
 
+#Close Out Ports
 ESPserial.close()
 ArdSerial.close()
+
+# #Write and Read to ESP
+# ESPserial.write(b'hello')
+# s = ESPserial.read(5)
+# print(s)
+# #Write To Arduino
+# ArdSerial.write(b'hello')
+
